49e0f7f8eec49b43079448d89976b1ca6c820e59
Ioakeim Mingo <2106069@brunel.ac.uk>
2024-07-12 20:27:53 +0100

    Add sorting algorithm context and provider, init utils

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
+"use client";
+
+import { useSortingAlgorithmContext } from "@/context/Visualiser";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { arrayToSort, isSorting } = useSortingAlgorithmContext();
+  useEffect(() => {
+    console.log(arrayToSort);
+    console.log(isSorting);
+  }, []);
   return <div>Hello</div>;
 }
